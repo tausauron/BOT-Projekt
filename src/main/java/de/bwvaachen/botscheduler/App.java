63bfd572296b2a09061ebbc115ef7e0b680b1f5f
unknown <Grassmann_Dus@K2AC.LOCAL>
2024-02-01 12:20:33 +0100

    Test1

--- src/main/java/de/bwvaachen/botscheduler/App.java
+++ src/main/java/de/bwvaachen/botscheduler/App.java
@@ -10,10 +10,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "new Branch" );
         System.out.println( "Test" );
-<<<<<<< HEAD
         System.out.println("Hallo Dustin");
-=======
-        
->>>>>>> a54ab4a974a711bb2616bf593c9ab36f4cf8aa2a
     }
 }
