8b1e5fa31ff9008ba441e7a2fd4bf298e6dc7383
unknown <wagner_eri@k2ac.local>
2024-02-01 12:10:23 +0100

    Hallo

--- src/main/java/de/bwvaachen/botscheduler/App.java
+++ src/main/java/de/bwvaachen/botscheduler/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println( "new Branch" );
+        System.out.println( "Test" );
+        System.out.println("Hallo Dustin");
     }
 }
