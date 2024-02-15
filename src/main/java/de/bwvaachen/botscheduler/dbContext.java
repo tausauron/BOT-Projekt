fce7d9054385e64c588cc1adf2020c59fb61c038
gabor_tom <gabortomi11@gmail.com>
2024-02-15 14:10:50 +0100

    db connection configuration

--- src/main/java/de/bwvaachen/botscheduler/dbContext.java
+++ src/main/java/de/bwvaachen/botscheduler/dbContext.java
@@ -1,10 +1,12 @@
 package de.bwvaachen.botscheduler;
 import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class dbContext {
     public static void main(String[] args) throws Exception {
-            Connection con = Connection
-            Connection conn = DriverManager. getConnection("jdbc:h2:~/test", "sa", "");
+
+            Connection conn = DriverManager.getConnection("jdbc:h2:C/Users/gabor_tom/test", "sa", "");
             //...
             conn.close();
         }
