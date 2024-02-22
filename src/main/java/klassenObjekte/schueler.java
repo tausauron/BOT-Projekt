7c000c39f6e1ac0430dc5f1e5c01792fc37e58db
Martin A <Martin.albertz@mitgmbh.de>
2024-02-22 09:08:14 +0100

    Klassen wurden überarbeitet

--- src/main/java/klassenObjekte/schueler.java
+++ src/main/java/klassenObjekte/schueler.java
@@ -1,16 +1,24 @@
 package klassenObjekte;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class schueler
 {
 	private int schuelerID;
 	private String vorname, nachname;
-	private ArrayList<String> wuensche;
+	private List<String> wuensche;
 	private String klasse;
-	
-	
-	
+
+
+	public schueler(String klasse,String vorname, String nachname,List<String> wuensche)
+	{
+		setKlasse(klasse);
+		setVorname(vorname);
+		setNachname(nachname);
+		setAllWuensche(getAllWuensche());
+	}
+
 	public int getSchuelerID()
 	{
 		return schuelerID;
@@ -25,7 +33,14 @@ public class schueler
 	}
 	public void setVorname(String vorname)
 	{
-		this.vorname = vorname;
+		if(vorname==null)
+		{
+			throw new IllegalArgumentException("Vorname darf nicht leer sein");
+		}else
+		{
+			this.vorname = vorname;
+		}
+
 	}
 	public String getNachname()
 	{
@@ -33,15 +48,39 @@ public class schueler
 	}
 	public void setNachname(String nachname)
 	{
-		this.nachname = nachname;
+		if(nachname==null)
+		{
+			throw new IllegalArgumentException("Nachname darf nicht leer sein");
+		}else
+		{
+			this.nachname = nachname;
+		}
 	}
-	public ArrayList<String> getWuensche()
+	public List<String> getAllWuensche()
 	{
 		return wuensche;
 	}
-	public void setWuensche(ArrayList<String> wuensche)
+
+	public String getWunsch(int i)
 	{
-		this.wuensche = wuensche;
+		if(i>this.wuensche.size())
+		{
+			throw new IllegalArgumentException("Eingegebener Wert ist außerhalb der Werte");
+		}else
+		{
+			return wuensche.get(i);
+		}
+	}
+
+	public void setAllWuensche(List<String> list)
+	{
+		if(list==null)
+		{
+			throw new IllegalArgumentException("Wünsche darf nicht leer sein");
+		}else
+		{
+			this.wuensche = list;
+		}
 	}
 	public String getKlasse()
 	{
@@ -49,6 +88,12 @@ public class schueler
 	}
 	public void setKlasse(String klasse)
 	{
-		this.klasse = klasse;
+		if(klasse==null)
+		{
+			throw new IllegalArgumentException("Klasse darf nicht leer sein");
+		}else
+		{
+			this.klasse = klasse;
+		}
 	}
 }
