b58803d665166f13499765f1d1fc0316ee014690
gabor_tom <gabortomi11@gmail.com>
2024-02-22 12:23:32 +0100

    h2 library version changed in pom
    in dbContext created database connection
    test table and test data inserted in database

--- src/main/java/de/bwvaachen/botscheduler/dbContext.java
+++ src/main/java/de/bwvaachen/botscheduler/dbContext.java
@@ -2,12 +2,33 @@ package de.bwvaachen.botscheduler;
 import java.sql.*;
 import java.util.Properties;
 import java.util.logging.Logger;
+import java.lang.ClassLoader;
+
 
 public class dbContext {
     public static void main(String[] args) throws Exception {
 
-            Connection conn = DriverManager.getConnection("jdbc:h2:C/Users/gabor_tom/test", "sa", "");
-            //...
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            String sqlCreate = "DROP TABLE IF EXISTS Persons;" +
+                    "CREATE TABLE Persons (" +
+                    "    PersonID int," +
+                    "    LastName varchar(255)," +
+                    "    FirstName varchar(255)," +
+                    "    Address varchar(255)," +
+                    "    City varchar(255)" +
+                    ");";
+            String sqlInsert = "INSERT INTO Persons VALUES (2, 'Gabor', 'Tomas', 'Bla', 'Aachen');";
+            String sql = "SELECT * FROM Persons;";
+
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(sqlCreate);
+            statement.executeUpdate(sqlInsert);
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            resultSet.next();
+            System.out.println(resultSet.getString("PersonID"));
+
             conn.close();
         }
     }
