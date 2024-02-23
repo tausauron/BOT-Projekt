2390224d4061bcd95b9cdad2db27b8eaca4dbf67
gabor_tom <gabortomi11@gmail.com>
2024-02-23 10:03:25 +0100

    dbModel start

--- src/main/java/de/bwvaachen/botscheduler/database/dbContext.java
+++ src/main/java/de/bwvaachen/botscheduler/database/dbContext.java
@@ -10,15 +10,37 @@ public class dbContext {
     public static void main(String[] args) throws Exception {
 
         Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-        String sqlCreate = "DROP TABLE IF EXISTS Persons;" +
-                "CREATE TABLE Persons (" +
-                "    PersonID int," +
-                "    LastName varchar(255)," +
-                "    FirstName varchar(255)," +
-                "    Address varchar(255)," +
-                "    City varchar(255)" +
+        Connection conn = DriverManager.getConnection("jdbc:h2:H:/ProjektRepos/BOT-Projekt/src/main/resources/test", "sa", "");
+
+        String sqlCreateTblSchueler = "DROP TABLE IF EXISTS Schueler;" +
+                "CREATE TABLE Schueler (" +
+                "    schuelerID int," +
+                "    nachname varchar(50)," +
+                "    vorname varchar(50)," +
+                "    wunsch1 int," +
+                "    wunsch2 int," +
+                "    wunsch3 int," +
+                "    wunsch4 int," +
+                "    wunsch5 int," +
+                "    wunsch6 int," +
+                "    klasse varchar(50)" +
                 ");";
+
+        String sqlCreateTblUnternehmen = "DROP TABLE IF EXISTS Unternehmen;" +
+                "CREATE TABLE Unternehmen (" +
+                "    firmenID int," +
+                "    unternehmenName varchar(50)," +
+                "    fachrichtung varchar(50)," +
+                "    maxTeilnehmer int," +
+                "    maxVeranstaltungen int," +
+                "    fruehsterZeitSlot int," +
+                "    gewichtung double," + // double als datentyp näher anschauen was notwending
+                "    aktiv boolean," +
+                "    wunsch6 int," +
+                "    klasse varchar(50)" +
+                ");";
+
+
         String sqlInsert = "INSERT INTO Persons VALUES (2, 'Gabor', 'Tomas', 'Bla', 'Aachen');";
         String sql = "SELECT * FROM Persons;";
 
