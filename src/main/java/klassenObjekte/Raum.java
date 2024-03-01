6ccaab49e3b648269fe4993fc2ff6109ffc32d3e
Martin Albertz <65467699+KnightVanKumAlot@users.noreply.github.com>
2024-03-01 13:55:07 +0100

    Sprint2 albertz (#21)
    * Raum Klasse angepasst
    
    * Raum Korregiert
    
    ---------
    
    Co-authored-by: Martin A <Martin.albertz@mitgmbh.de>

--- src/main/java/klassenObjekte/Raum.java
+++ src/main/java/klassenObjekte/Raum.java
@@ -1,36 +1,33 @@
 package klassenObjekte;
 
-<<<<<<< HEAD
+
+
 /**
  * 
  * @author Martin Albertz
  * 
  * 
- * TODO Liste mit Zeitslots hinzufuegen
+ *  TODO Liste mit Zeitslots hinzufuegen
  */
 public class Raum
 {
 	private String name;
 	private int kapazitaet;
-	
+
+
 	public Raum(String name, int kapazitaet)
 	{
 		// TODO Auto-generated constructor stub
 		setKapazitaet(kapazitaet);
 		setName(name);
-=======
-public class Raum {
 
-	String name;
-
-	public Raum(String name) {
-		this.name = name;
->>>>>>> branch 'Sprint2' of https://github.com/tausauron/BOT-Projekt.git
-	}
+  }
+    
 	public String getName()
 	{
 		return name;
 	}
+
 	public void setName(String name)
 	{
 		this.name = name;
@@ -43,8 +40,6 @@ public class Raum {
 	{
 		this.kapazitaet = kapazitaet;
 	}
-	
 
-	
 
 }
