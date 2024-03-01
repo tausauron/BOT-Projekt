5614c5a145cfafd396a25f01e0c47a370e5d3395
Martin A <Martin.albertz@mitgmbh.de>
2024-03-01 11:02:25 +0100

    Raum Klasse angepasst

--- src/main/java/klassenObjekte/Raum.java
+++ src/main/java/klassenObjekte/Raum.java
@@ -1,19 +1,41 @@
 package klassenObjekte;
 
-public class Raum {
-
-	String name;
-
-	Raum(String name) {
-		this.name = name;
+/**
+ * 
+ * @author Martin Albertz
+ * 
+ * 
+ * TODO Liste mit Zeitslots hinzufuegen
+ */
+public class Raum
+{
+	private String name;
+	private int kapazitaet;
+	
+	public Raum(String name, int kapazitaet)
+	{
+		// TODO Auto-generated constructor stub
+		setKapazitaet(kapazitaet);
+		setName(name);
 	}
-
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
-
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		this.name = name;
 	}
+	public int getKapazitaet()
+	{
+		return kapazitaet;
+	}
+	public void setKapazitaet(int kapazitaet)
+	{
+		this.kapazitaet = kapazitaet;
+	}
+	
+
+	
 
 }
