fc4b7d97fda52906318e1bb35c448f7065a6975b
Max Tautenhahn <136613391+tausauron@users.noreply.github.com>
2024-03-06 22:07:16 +0100

    algorithm v2 (#22)
    * begrenzung Raumanzahl
    
    * max Veranstaltungen
    
    * swapping
    
    * update Model

--- src/main/java/de/bwvaachen/botscheduler/model/Model.java
+++ src/main/java/de/bwvaachen/botscheduler/model/Model.java
@@ -7,6 +7,7 @@ import de.bwvaachen.botscheduler.calculate.KursPlaner;
 import de.bwvaachen.botscheduler.grassmann.myInterface.ModelInterface;
 import execlLoad.ImportFile;
 import klassenObjekte.Kurse;
+import klassenObjekte.Raum;
 import klassenObjekte.Schueler;
 import klassenObjekte.Unternehmen;
 
@@ -20,6 +21,7 @@ public class Model implements ModelInterface{
 	List<Schueler> schueler = new ArrayList<>();
 	List<Kurse> kurse = new ArrayList<>();
 	List<Unternehmen> unternehmen = new ArrayList<>();
+	List<Raum> raeume = new ArrayList<>();
 
 	@Override
 	public Boolean checkLogin(String username, String password) {
@@ -29,15 +31,10 @@ public class Model implements ModelInterface{
 	
 	public void belegeKurse() {
 		KursPlaner planer = new KursPlaner();
-		String score = planer.belegeKurse(schueler, unternehmen);
+		String score = planer.belegeKurse(schueler, unternehmen, raeume);
 		kurse = planer.getKurse();
 	}
 
-	@Override
-	public Schueler getStudent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public void createStudent(int schuelerID, String vorname, String nachname, ArrayList<String> wuensche,
@@ -48,24 +45,12 @@ public class Model implements ModelInterface{
 
 
 	@Override
-	public void exportStudent() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void createCompany(String firmenName, int firmenID, int maxTeilnehmer, ArrayList<Integer> zeitslots,
 			double gewichtung, boolean aktiv) {
 		// TODO Auto-generated method stub
 		
 	}
 
-
-	@Override
-	public void exportCompany() {
-		// TODO Auto-generated method stub
-		
-	}
 
 
 	@Override
@@ -97,10 +82,89 @@ public class Model implements ModelInterface{
 		return ImportFile.getChoices(absolutePath);
 	}
 
+
 	@Override
-	public List<Unternehmen> importCompany() {
+	public List<Schueler> getAllStudents() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void saveAllStudents(List<Schueler> students) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exportStudent(String path, List<Schueler> students) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Raum> getAllRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveAllRooms(List<Raum> rooms) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createRoom(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editRoom(Raum room) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRoom(Raum room) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Raum> importRooms(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exportRooms(String path, List<Raum> rooms) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Unternehmen> getAllCompanies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveAllCompanies(List<Unternehmen> companies) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Unternehmen> importCompany(String absolutePath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exportCompany(String path, List<Unternehmen> companies) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
