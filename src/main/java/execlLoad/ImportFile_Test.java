6d03b40e0331e59229beb8aa5c49237f095e1c2c
Nani446 <naemadarbash80@gmail.com>
2024-03-08 11:04:29 +0100

    Test klasse für ImportFile erstellt

--- src/main/java/execlLoad/ImportFile_Test.java
+++ src/main/java/execlLoad/ImportFile_Test.java
@@ -1,4 +1,5 @@
 package execlLoad;
+import klassenObjekte.Raum;
 import klassenObjekte.Schueler;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -13,9 +14,11 @@ import java.util.List;
 public class ImportFile_Test {
     public static void main(String[] args) {
         testGetChoices_ValidFile();
-        //testGetCompany_ValidFile();
+        testGetCompany_ValidFile();
+        testGetRoom_ValidFile();
     }
 
+        //Test für GetChoices
         @Test
         static void testGetChoices_ValidFile() throws IllegalArgumentException{
             // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
@@ -45,6 +48,17 @@ public class ImportFile_Test {
                 assertTrue( "Die Methode sollte keine IllegalArgumentException werfen.", false);
             }
         }
+
+    //Test für GetChoices
+    @Test
+    static void testGetChoices_InValidFileFormat() {
+        // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
+        String path = "H:\\SUD\\IMPORT BOT2_Wahl.xlsx";
+        IllegalArgumentException th
+
+    }
+
+        // Test für GetCompany
     @Test
     static void testGetCompany_ValidFile() throws IllegalArgumentException{
         // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
@@ -56,15 +70,15 @@ public class ImportFile_Test {
             // Ersetzen Sie diese Überprüfungen durch spezifische Tests basierend auf dem Inhalt Ihrer Testdatei
             assertFalse( "Die Liste sollte nicht leer sein.", unternehmenList.isEmpty());
 
-            // Überprüfen Sie, ob der erste Schüler in der Liste den erwarteten Werten entspricht
-            //ersten Unternehmen : 1	Zentis	Industriekaufleute	20	5	A
+            // Überprüfen Sie, ob der erste Unternehmen in der Liste den erwarteten Werten entspricht
+            //Values des ersten Unternehmens : 1,	Zentis,	Industriekaufleute,	20,	5,	A
             Unternehmen ersterUnternehmen = unternehmenList.get(0);
-            assertEquals("ID der Firme des ersten Unternehmen stimmt nicht überein.", ersterUnternehmen.getFirmenID(), "1");
-            assertEquals("Unternehmen des ersten Schülers Unternehmen nicht überein.", ersterUnternehmen.getUnternehmen().toString(), "Zentis");
-            assertEquals("Fachrichtung des ersten Schülers Unternehmen nicht überein.", ersterUnternehmen.getFachrichtung().toString(), "Industriekaufleute");
-            assertEquals("MaxTeilnehmer des ersten Schülers Unternehmen nicht überein.", ersterUnternehmen.getMaxTeilnehmer(), "20");
-            assertEquals("MaxVeranstaltungen des ersten Unternehmen stimmt nicht überein.", ersterUnternehmen.getMaxVeranstaltungen(), "5");
-            assertEquals("FruehesterZeitslot des ersten Unternehmen stimmt nicht überein.", ersterUnternehmen.getFruehesterZeitslot().toString(), "A");
+            assertEquals("ID der Firme des ersten Unternehmens stimmt nicht überein.", String.valueOf(ersterUnternehmen.getFirmenID()), "1");
+            assertEquals("Unternehmen des ersten  Unternehmens nicht überein.", ersterUnternehmen.getUnternehmen().toString(), "Zentis");
+            assertEquals("Fachrichtung des ersten  Unternehmens nicht überein.", ersterUnternehmen.getFachrichtung().toString(), "Industriekaufleute");
+            assertEquals("MaxTeilnehmer des ersten  Unternehmens nicht überein.", String.valueOf(ersterUnternehmen.getMaxTeilnehmer()), "20");
+            assertEquals("MaxVeranstaltungen des ersten Unternehmens stimmt nicht überein.", String.valueOf(ersterUnternehmen.getMaxVeranstaltungen()), "5");
+            assertEquals("FruehesterZeitslot des ersten Unternehmens stimmt nicht überein.", ersterUnternehmen.getFruehesterZeitslot().toString(), "A");
 
 
         } catch (IllegalArgumentException e) {
@@ -72,6 +86,27 @@ public class ImportFile_Test {
         }
 
     }
+    // Test für GetRoom
+    @Test
+    static void testGetRoom_ValidFile() throws IllegalArgumentException{
+        // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
+        String path = "H:\\SUD\\IMPORT BOT0_Raumliste.xlsx";
+
+        try {
+            List<Raum> RaumList = ImportFile.getRoom(path);
+
+            // Ersetzen Sie diese Überprüfungen durch spezifische Tests basierend auf dem Inhalt Ihrer Testdatei
+            assertFalse( "Die Liste sollte nicht leer sein.", RaumList.isEmpty());
+
+            // Überprüfen Sie, ob der erste Raum in der Liste den erwarteten Werten entspricht
+            Raum ersteRaum = RaumList.get(0);
+            assertEquals("Der Name des ersten Raums stimmt nicht überein.", ersteRaum.getName().toString(), "008");
+            assertEquals("Die Kapazität des ersten Raums stimmt nicht überein.", String.valueOf(ersteRaum.getKapazitaet()), "15");
 
 
+        } catch (IllegalArgumentException e) {
+            assertTrue( "Die Methode sollte keine IllegalArgumentException werfen.", false);
+        }
+
+    }
 }
