a5a6129ddb0a1d8c9d1af19520409bba828a4448
Nani446 <naemadarbash80@gmail.com>
2024-03-14 09:21:41 +0100

    Unittest implementieren

--- src/test/java/de/bwvaachen/botscheduler/TestImportFile.java
+++ src/test/java/de/bwvaachen/botscheduler/TestImportFile.java
@@ -6,6 +6,7 @@ import klassenObjekte.Schueler;
 import klassenObjekte.Unternehmen;
 import org.junit.Test;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -13,17 +14,17 @@ import static org.junit.Assert.*;
 
 
 public class TestImportFile {
-    public static void main(String[] args) {
-        testGetChoices_ValidFile();
-      //  testGetCompany_ValidFile();
-      //  testGetRoom_ValidFile();
+    public static void main(String[] args)throws IllegalArgumentException, URISyntaxException {
+         testGetChoices_ValidFile();
+        testGetCompany_ValidFile();
+        testGetRoom_ValidFile();
     }
 
         //Test für GetChoices
         @Test
-        static void testGetChoices_ValidFile() throws IllegalArgumentException{
+        static void testGetChoices_ValidFile() throws IllegalArgumentException, URISyntaxException {
             // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
-            String path =ImportFile.class.getResource("IMPORT BOT2_Wahl.xlsx").toURI().getPath();;
+            String path =ImportFile.class.getResource("IMPORT BOT2_Wahl.xlsx").toURI().getPath();
 
 
             try {
@@ -40,31 +41,25 @@ public class TestImportFile {
 
                 // Überprüfen der Wünsche des ersten Schülers, wenn bekannt
                 assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("6"));
-                assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("18"));
-                assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("19"));
-                assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("4"));
-                assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("14"));
-                assertTrue( "Die Wunschliste des ersten Schülers sollte '3' enthalten.", ersterSchueler.getAllWuensche().contains("22"));
+                assertTrue( "Die Wunschliste des ersten Schülers sollte '4' enthalten.", ersterSchueler.getAllWuensche().contains("18"));
+                assertTrue( "Die Wunschliste des ersten Schülers sollte '5' enthalten.", ersterSchueler.getAllWuensche().contains("19"));
+                assertTrue( "Die Wunschliste des ersten Schülers sollte '6' enthalten.", ersterSchueler.getAllWuensche().contains("4"));
+                assertTrue( "Die Wunschliste des ersten Schülers sollte '7' enthalten.", ersterSchueler.getAllWuensche().contains("14"));
+                assertTrue( "Die Wunschliste des ersten Schülers sollte '8' enthalten.", ersterSchueler.getAllWuensche().contains("22"));
 
             } catch (IllegalArgumentException e) {
                 assertTrue( "Die Methode sollte keine IllegalArgumentException werfen.", false);
             }
         }
 
-    //Test für GetChoices
-    @Test
-    static void testGetChoices_InValidFileFormat() {
-        // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
-        String path = "H:\\SUD\\IMPORT BOT2_Wahl.xlsx";
 
 
-    }
 
         // Test für GetCompany
     @Test
-    static void testGetCompany_ValidFile() throws IllegalArgumentException{
+    static void testGetCompany_ValidFile() throws IllegalArgumentException, URISyntaxException {
         // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
-        String path = "H:\\SUD\\IMPORT BOT1_Veranstaltungsliste.xlsx";
+        String path =ImportFile.class.getResource("IMPORT BOT1_Veranstaltungsliste.xlsx").toURI().getPath();
 
         try {
             List<Unternehmen> unternehmenList = ImportFile.getCompany(path);
@@ -73,7 +68,7 @@ public class TestImportFile {
             assertFalse( "Die Liste sollte nicht leer sein.", unternehmenList.isEmpty());
 
             // Überprüfen Sie, ob der erste Unternehmen in der Liste den erwarteten Werten entspricht
-            //Values des ersten Unternehmens : 1,	Zentis,	Industriekaufleute,	20,	5,	A
+            //Values des ersten Unternehmens : 1/Zentis/Industriekaufleute/20/5/A
             Unternehmen ersterUnternehmen = unternehmenList.get(0);
             assertEquals("ID der Firme des ersten Unternehmens stimmt nicht überein.", String.valueOf(ersterUnternehmen.getFirmenID()), "1");
             assertEquals("Unternehmen des ersten  Unternehmens nicht überein.", ersterUnternehmen.getUnternehmen().toString(), "Zentis");
@@ -88,11 +83,12 @@ public class TestImportFile {
         }
 
     }
+
     // Test für GetRoom
     @Test
-    static void testGetRoom_ValidFile() throws IllegalArgumentException{
+    static void testGetRoom_ValidFile() throws IllegalArgumentException, URISyntaxException {
         // Ersetzen Sie diesen Pfad durch den Pfad zu Ihrer Test-Excel-Datei
-        String path = "H:\\SUD\\IMPORT BOT0_Raumliste.xlsx";
+            String path =ImportFile.class.getResource("IMPORT BOT0_Raumliste.xlsx").toURI().getPath();
 
         try {
             List<Raum> RaumList = ImportFile.getRoom(path);
@@ -104,7 +100,6 @@ public class TestImportFile {
             Raum ersteRaum = RaumList.get(0);
             assertEquals("Der Name des ersten Raums stimmt nicht überein.", ersteRaum.getName().toString(), "008");
             assertEquals("Die Kapazität des ersten Raums stimmt nicht überein.", String.valueOf(ersteRaum.getKapazitaet()), "15");
-
 
         } catch (IllegalArgumentException e) {
             assertTrue( "Die Methode sollte keine IllegalArgumentException werfen.", false);
