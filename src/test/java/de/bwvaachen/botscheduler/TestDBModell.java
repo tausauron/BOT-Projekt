f62882ab63d5e74a131e5396e1984415818c40a9
gabor_tom <gabortomi11@gmail.com>
2024-03-14 11:39:37 +0100

    test methoden für Input Methoden

--- src/test/java/de/bwvaachen/botscheduler/TestDBModell.java
+++ src/test/java/de/bwvaachen/botscheduler/TestDBModell.java
@@ -170,6 +170,36 @@ public class TestDBModell {
         }
     }
 
+    @Test
+    void testSaveKurse() throws SQLException, ClassNotFoundException {
+        Connection conn = database.connection();
+        database.saveRooms(raum);
+        database.saveSchueler(schueler);
+        database.saveUnternehmen(unternehmenDAO);
+        database.saveKurse(kurseDAO);
+
+        String getRow = "SELECT kursID, raumID, firmenID FROM Kurs;";
+        String getKursTeilnehmer = "SELECT KursTeilnehmer.schuelerID, Schueler.vorname, Schueler.nachname " +
+                                    "FROM KursTeilnehmer " +
+                                    "INNER JOIN Schueler ON KursTeilnehmer.schuelerID = Schueler.schuelerID;";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(getRow);
+        Statement statementKursTeilnehmer = conn.createStatement();
+        ResultSet resKursTeilnehmer = statementKursTeilnehmer.executeQuery(getKursTeilnehmer);
+
+        while (resultSet.next() && resKursTeilnehmer.next()){
+            System.out.println("KursID: " + resultSet.getString("kursID"));
+            System.out.println("RaumID: " + resultSet.getString("raumID"));
+            System.out.println("FimenID: " + resultSet.getString("firmenID"));
+            System.out.println("SchuelerID: " + resKursTeilnehmer.getInt("schuelerID"));
+            System.out.println("Vorname: " + resKursTeilnehmer.getString("vorname"));
+            System.out.println("Nachname: " + resKursTeilnehmer.getString("nachname"));
+            System.out.println("----------------------------");
+
+            assertEquals(raum.contains(resultSet.getInt("raumID")),
+                    unternehmenDAO.contains(resultSet.getInt("firmenID")));
+        }
+    }
 
     @Test
     void testLoadKurse() throws SQLException, ClassNotFoundException {
@@ -181,4 +211,98 @@ public class TestDBModell {
 
         System.out.println("fertig");
     }
+
+    @Test
+    void testSaveSchuelerInputData() throws SQLException, ClassNotFoundException {
+        Connection conn = database.connection();
+        database.saveSchuelerInputData(schueler);
+
+        String getRow = "SELECT nachname,wunsch1,wunsch2,wunsch3 FROM SchuelerInput;";
+        String getRowCount = "SELECT COUNT(nachname) FROM SchuelerInput;";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(getRow);
+
+        Statement rowCount = conn.createStatement();
+        //ResultSet resultSetCount = statement.executeQuery(getRowCount);
+
+        for (int i = 0; resultSet.next(); i++){
+
+            System.out.println(resultSet.getString("nachname"));
+            System.out.println(resultSet.getString("wunsch1"));
+            System.out.println(resultSet.getString("wunsch2"));
+            System.out.println(resultSet.getString("wunsch3"));
+            assertEquals(schueler.get(i).getNachname(), resultSet.getString("nachname"));
+        }
+    }
+    @Test
+    void testLoadSchuelerInputData() throws SQLException, ClassNotFoundException {
+        database.saveSchuelerInputData(schueler);
+        List<Schueler> testList = database.loadSchuelerInput();
+
+        for (int i = 0; i < schueler.size(); i++) {
+            assertEquals(schueler.get(i).getVorname(), testList.get(i).getVorname());
+        }
+    }
+
+    @Test
+    void testSaveUnternehmenInputData() throws SQLException, ClassNotFoundException {
+        Connection conn = database.connection();
+        database.saveUnternehmenInputData(unternehmenDAO);
+
+        String getRow = "SELECT unternehmenName FROM UnternehmenInput;";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(getRow);
+
+        for (int i = 0; resultSet.next(); i++){
+            System.out.println("Input Unternehmen aus DB: " + resultSet.getString("unternehmenName"));
+            System.out.println("Unternehmen aus Excel: " + unternehmen.get(i).getUnternehmen());
+            System.out.println("----------------------------");
+            assertEquals(unternehmen.get(i).getUnternehmen(), resultSet.getString("unternehmenName"));
+        }
+    }
+
+    @Test
+    void testLoadUnternehmenInput() throws SQLException, ClassNotFoundException {
+        database.saveUnternehmenInputData(unternehmenDAO);
+        List<UnternehmenDAO> testList = database.loadUnternehmenInput();
+
+        for (int i = 0; i < unternehmen.size(); i++) {
+            System.out.println("Input Unternehmen aus DB: " + testList.get(i).getUnternehmen());
+            System.out.println("Unternehmen aus Excel: " + unternehmen.get(i).getUnternehmen());
+            System.out.println("----------------------------");
+            assertEquals(unternehmen.get(i).getUnternehmen(), testList.get(i).getUnternehmen());
+        }
+    }
+
+
+    @Test
+    void testSaveRoomInputData() throws SQLException, ClassNotFoundException {
+        Connection conn = database.connection();
+        database.saveRoomInputData(raum);
+
+        String getRow = "SELECT name FROM RaumInput;";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(getRow);
+
+        for (int i = 0; resultSet.next(); i++){
+            System.out.println("Input Raum aus DB: " + resultSet.getString("name"));
+            System.out.println("Raum aus Excel: " + raum.get(i).getName());
+            System.out.println("----------------------------");
+            assertEquals(raum.get(i).getName(), resultSet.getString("name"));
+        }
+    }
+
+    @Test
+    void testLoadRoomsInput() throws SQLException, ClassNotFoundException {
+        database.saveRoomInputData(raum);
+        List<Raum> testList = database.loadRoomsInput();
+
+        for (int i = 0; i < testList.size(); i++) {
+            System.out.println("Input Raum aus DB: " + testList.get(i).getName());
+            System.out.println("Raum aus Excel: " + raum.get(i).getName());
+            System.out.println("----------------------------");
+            assertEquals(raum.get(i).getName(), testList.get(i).getName());
+        }
+    }
+
 }
