41e6b4873835f850671d8d6f042c97bcdf834b38
Max Tautenhahn <max.tautenhahn@rwth-aachen.de>
2024-03-17 22:40:20 +0100

    cleanup for release 2, shaded jar

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 
-%JAVA_HOME%\bin\javaw -cp botscheduler-1.0.0.jar;dependency/* de.bwvaachen.botscheduler.App
+%JAVA_HOME%\bin\javaw -cp botscheduler-1.0.0.jar de.bwvaachen.botscheduler.App
 ::pause
